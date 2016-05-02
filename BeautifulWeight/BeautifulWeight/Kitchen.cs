9c26f90ddd708afe66c76f1927f442bbb6d0eedd
Federico Baldassarre <baldassarre.fe@gmail.com>
2016-05-02 13:36:31 +0200

    Fatti namespace Kitchen e Menu

---
+++ BeautifulWeight/BeautifulWeight/Kitchen.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautifulWeight.Kitchen
+{
+    class Ingredient
+    {
+    private readonly string _name;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+    }
+
+    class Dish
+    {
+        private readonly string _name;
+        private readonly double _calories;
+        private readonly double _proteins;
+        private readonly double _fats;
+        private readonly double _carbohydrates;
+        private readonly NonEmptyList<Ingredient> _ingredients;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        public double Calories
+        {
+            get
+            {
+                return _calories;
+            }
+        }
+
+        public double Proteins
+        {
+            get
+            {
+                return _proteins;
+            }
+        }
+
+        public double Fats
+        {
+            get
+            {
+                return _fats;
+            }
+        }
+
+        public double Carbohydrates
+        {
+            get
+            {
+                return _carbohydrates;
+            }
+        }
+
+        public NonEmptyList<Ingredient> Ingredients
+        {
+            get
+            {
+                return _ingredients;
+            }
+        }
+    }
+}
---
+++ BeautifulWeight/BeautifulWeight/Menu.cs
@@ -0,0 +1,107 @@
+﻿using BeautifulWeight.Kitchen;
+using BeautifulWeight.DietCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautifulWeight.Menu
+{
+    class Serving
+    {
+        private readonly int _quantity;
+        private readonly Dish _dish;
+
+        public int Quantity
+        {
+            get
+            {
+                return _quantity;
+            }
+        }
+
+        internal Dish Dish
+        {
+            get
+            {
+                return _dish;
+            }
+        }
+    }
+
+    class Meal
+    {
+        private readonly string _name;
+        private readonly DateTime _time;
+        private readonly NonEmptyList<Serving> _servings;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        public DateTime Time
+        {
+            get
+            {
+                return _time;
+            }
+        }
+
+        NonEmptyList<Serving> Servings
+        {
+            get
+            {
+                return _servings;
+            }
+        }
+    }
+
+    class DailyMenu
+    {
+        private readonly DayOfWeek _day;
+        private readonly List<Meal> _meals;
+
+        public DayOfWeek Day
+        {
+            get
+            {
+                return _day;
+            }
+        }
+
+        internal List<Meal> Meals
+        {
+            get
+            {
+                return _meals;
+            }
+        }
+    }
+
+    class WeeklyMenu
+    {
+        private readonly DailyMenu[] _dailyMenu;
+        private readonly DietCalculator _dietCalculator;
+
+        public DailyMenu this[DayOfWeek day]
+        {
+            get
+            {
+                return _dailyMenu[(int) day];
+            }
+        }
+
+        public DietCalculator DietCalculator
+        {
+            get
+            {
+                return _dietCalculator;
+            }
+        }
+    }
+}
