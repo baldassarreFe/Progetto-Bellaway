0e819669e4f1af0588c701a9f9c7d3350928fae0
Federico Venturini <venturini.fe@gmail.com>
2016-05-26 10:58:13 +0200

    Version e VersionManager

---
+++ BeautifulWeight/BeautifulWeight/Model/Version.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautifulWeight.Model
+{
+    partial class Version
+    {
+        private class StandardVersion : Version
+        {
+
+        }
+
+        private class PremiumVersion : Version
+        {
+
+        }
+    }
+}
---
+++ BeautifulWeight/BeautifulWeight/Model/VersionManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautifulWeight.Version
+{
+    class VersionManager
+    {
+        private Version _version;
+        public bool Allows(Feature feature)
+        {
+            return _version.Allows(feature);
+        }
+
+        
+        private abstract partial class Version
+        {
+            public abstract string Description { get; }
+            public abstract bool Allows(Feature feature);
+        }
+    }
+
+}
