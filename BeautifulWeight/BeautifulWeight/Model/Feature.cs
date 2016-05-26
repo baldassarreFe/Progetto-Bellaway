0d7c222abbe63613bc37ca980332540382ba458e
Gabriele Corni <gabriele.corni@gmail.com>
2016-05-26 10:56:26 +0200

    feature

---
+++ BeautifulWeight/BeautifulWeight/Model/Feature.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautifulWeight.Version
+{
+    enum Feature
+    {
+        ADD_USER, CHANGE_GOAL, CHANGE_DIET, CHANGE_SERVING, VIEW_NEXT_MEAL
+    }
+}
