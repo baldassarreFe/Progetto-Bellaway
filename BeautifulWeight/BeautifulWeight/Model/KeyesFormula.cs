056cf91fa94ea0531859450c4d0c144b109f3e14
Gabriele Corni <gabriele.corni@gmail.com>
2016-06-01 12:23:52 +0200

    strategy keyes

---
+++ BeautifulWeight/BeautifulWeight/Model/KeyesFormula.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeautifulWeight.Users;
+
+namespace BeautifulWeight.Model
+{
+    class KeyesFormula : TargetWeightFormula
+    {
+        public double calculate(Sex sex, int height_cm)
+        {
+            double height_m = height_cm * 1.0 / 100;
+            double factor = Math.Pow(height_m, 2);
+            double mul = (sex == Sex.MALE) ? 22.1 : 20.6;
+            return factor * mul;
+        }
+    }
+}
